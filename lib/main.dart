9091383c4fe0180a870b2c37b4529391b1f0d1c5
migue-dev-web <miguel.castellanos@udgvirtual.udg.mx>
2025-05-03 12:47:44 -0600

    Act 3.6. Peticiones HTTP Completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -20,42 +22,87 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Act LDSW -3.5. Home screen"),
-      ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("lib/338492-PADRNY-313.jpg"),
-              fit: BoxFit.cover,
-              ),
-            ),
-          ),
-
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("Bienvenido a", style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,),
-                ),
-                SizedBox(height: 10),
-                Text( 'Mi Aplicación Flutter',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black87,
-                  ),)
-              ],
-            ),
-          ),
-
-
-        ],
-      ),
+    return MaterialApp(
+      title: 'CONSULTAS HTTP',
+      home: PokemonScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
+}
+
+class PokemonScreen extends StatefulWidget{
+  @override
+  _PokemonScreenState createState() => _PokemonScreenState(); 
+}
+
+class _PokemonScreenState extends State<PokemonScreen>{
+  String nombre = "";
+  String imgUrl = "";
+  bool cargando =true;
+  
+  @override
+  void initState() {
+    super.initState();
+    obtenerPokemon();
+    
+  }
+  
+  Future<void> obtenerPokemon() async{
+    final respuesta = 
+        await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon/pikachu"));
+
+    if (respuesta.statusCode == 200){
+      final datos = jsonDecode(respuesta.body);
+      setState(() {
+        nombre = datos["name"];
+        imgUrl = datos["sprites"]["front_default"];
+        cargando = false;
+      });
+    }else{
+      throw Exception("Error al cargar");
+    }
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(
+  title: Text("Act LDSW -3.6. Peticiones HTTP"),
+  ),
+  body: Stack(
+  children: [
+  Container(
+  decoration: BoxDecoration(
+  image: DecorationImage(image: AssetImage("lib/338492-PADRNY-313.jpg"),
+  fit: BoxFit.cover,
+  ),
+  ),
+  ),
+
+  Center(
+  child: cargando 
+    ? CircularProgressIndicator()
+      : Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+  Text(nombre.toUpperCase(), style: TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,),
+  ),
+  SizedBox(height: 10),
+  Image.network(imgUrl,  width: 200,
+    height: 200,
+    fit: BoxFit.contain,)
+  ],
+  ),
+  ),
+
+
+  ],
+  ),
+  );
+  }
+
+
 }
