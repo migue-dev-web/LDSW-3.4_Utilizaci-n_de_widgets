1ad786272743140739597d6a991671e27a55c878
migue-dev-web <miguel.castellanos@udgvirtual.udg.mx>
2025-05-10 21:54:06 -0600

    Act 3.7.Integración de base de datos completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // Asegura la inicialización antes de correr la app
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp()); // Corre la app normalmente
 }
 
 class MyApp extends StatelessWidget {
@@ -12,97 +19,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
       title: 'CONSULTAS HTTP',
-      home: PokemonScreen(),
+      home: const PokemonScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class PokemonScreen extends StatefulWidget{
+class PokemonScreen extends StatefulWidget {
+  const PokemonScreen({super.key});
+
   @override
-  _PokemonScreenState createState() => _PokemonScreenState(); 
+  _PokemonScreenState createState() => _PokemonScreenState();
 }
 
-class _PokemonScreenState extends State<PokemonScreen>{
+class _PokemonScreenState extends State<PokemonScreen> {
   String nombre = "";
   String imgUrl = "";
-  bool cargando =true;
-  
+  bool cargando = true;
+
   @override
   void initState() {
     super.initState();
     obtenerPokemon();
-    
   }
-  
-  Future<void> obtenerPokemon() async{
-    final respuesta = 
-        await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon/pikachu"));
 
-    if (respuesta.statusCode == 200){
+  Future<void> obtenerPokemon() async {
+    final respuesta =
+    await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon/pikachu"));
+
+    if (respuesta.statusCode == 200) {
       final datos = jsonDecode(respuesta.body);
       setState(() {
         nombre = datos["name"];
         imgUrl = datos["sprites"]["front_default"];
         cargando = false;
       });
-    }else{
-      throw Exception("Error al cargar");
+    } else {
+      throw Exception("Error al cargar Pokémon");
     }
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-  appBar: AppBar(
-  title: Text("Act LDSW -3.6. Peticiones HTTP"),
-  ),
-  body: Stack(
-  children: [
-  Container(
-  decoration: BoxDecoration(
-  image: DecorationImage(image: AssetImage("lib/338492-PADRNY-313.jpg"),
-  fit: BoxFit.cover,
-  ),
-  ),
-  ),
-
-  Center(
-  child: cargando 
-    ? CircularProgressIndicator()
-      : Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-  Text(nombre.toUpperCase(), style: TextStyle(
-  fontSize: 28,
-  fontWeight: FontWeight.bold,
-  color: Colors.black,),
-  ),
-  SizedBox(height: 10),
-  Image.network(imgUrl,  width: 200,
-    height: 200,
-    fit: BoxFit.contain,)
-  ],
-  ),
-  ),
-
-
-  ],
-  ),
-  );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Act LDSW -3.6. Peticiones HTTP"),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("lib/338492-PADRNY-313.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: cargando
+                ? const CircularProgressIndicator()
+                : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  nombre.toUpperCase(),
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Image.network(
+                  imgUrl,
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
-
-
 }
